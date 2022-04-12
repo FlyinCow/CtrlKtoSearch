6596649abc59342877ed5e20a35e962237eb0177
dbv <bowendean_cow@foxmail.com>
2022-04-12 22:13:52 +0800

    add MIT license

--- src/meta.js
+++ src/meta.js
@@ -4,6 +4,7 @@
 // @description This is a userscript.
 // @match       *://developer.mozilla.org/*
 // @grant       GM_addStyle
+// @license     MIT
 // @version     process.env.VERSION
 // @author      process.env.AUTHOR
 // @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
